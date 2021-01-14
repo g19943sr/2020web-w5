396d3065163cb3ed14951a052ee255dbe316cc02
g19943sr <75405783+g19943sr@users.noreply.github.com>
2021-01-14 12:27:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '今日は外では雪が降っていた。:insertx:は:inserty:へ行くために外へ出かけた。:inserty:へいく道中で、大きな池が凍っていた。:insertx:は、そこでスケートをしようと凍った池の上に乗っかると、:insertz:。:insertx: はあまりのショックに一回自分の家に帰ることにした。';
+let insertX = ['津田梅子さん','ドナルド','安倍晋三'];
+let insertY = ['友達の家','スーパー','恋人の家'];
+let insertZ = ['氷が割れて池に落っこちた','滑って転んでけがをした','池の管理人に怒られた'];
 
 randomize.addEventListener('click', result);
 
