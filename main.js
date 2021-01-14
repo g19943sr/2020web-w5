06ad019683508b2eeb8090ea2e92b5041564b6ba
g19943sr <75405783+g19943sr@users.noreply.github.com>
2021-01-14 12:37:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -26,18 +26,7 @@ function result() {
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
 
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace('Bob',name);
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
-  }
-
+  
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
